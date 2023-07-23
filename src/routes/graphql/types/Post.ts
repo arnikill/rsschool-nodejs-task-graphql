153bb8a3a79e55ba37cc7304a29959eb7340d317
aliya <tusupkalievaaliya921@gmail.com>
2023-07-23 21:31:52 +0500

    feat: add Post which defines the types of data

---
+++ src/routes/graphql/types/Post.ts
@@ -0,0 +1,43 @@
+import { GraphQLInputObjectType, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import { UUIDType } from "./uuid.js";
+import { Context, ID } from "./subscription.js";
+import { typeOfUser } from "./User.js";
+
+export interface AboutPost {
+    title: string;
+    content: string;
+    authorId: string;
+};
+interface Post extends AboutPost, ID { }
+
+export const typeOfPost = new GraphQLObjectType({
+    name: 'AboutPost',
+    fields: () => ({
+        id: { type: new GraphQLNonNull(UUIDType) },
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        content: { type: new GraphQLNonNull(GraphQLString) },
+        author: {
+            type: typeOfUser,
+            resolve: (source: Post, context: Context) => {
+                const loaders = context;
+                return loaders.userLoader.load(source.authorId);
+            },
+        }
+    })
+})
+export const developProfile = new GraphQLInputObjectType({//для создания нового профиля участника
+    name: 'DevelopProfile',
+    fields: {
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        content: { type: new GraphQLNonNull(GraphQLString) },
+        authorId: { type: new GraphQLNonNull(UUIDType) },
+    },
+});
+
+export const modifyProfile = new GraphQLInputObjectType({//для обновления  профиля участника
+    name: 'ModifyProfile',
+    fields: {
+        title: { type: (GraphQLString) },
+        content: { type: (GraphQLString) },
+    },
+});
