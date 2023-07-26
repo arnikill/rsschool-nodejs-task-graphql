6b9b51dce4ed70116960374898e2877a5b15683e
aliya <tusupkalievaaliya921@gmail.com>
2023-07-26 17:49:04 +0500

    refactor: add promises

--- src/routes/graphql/resolve/userResolve.ts
+++ src/routes/graphql/resolve/userResolve.ts
@@ -31,64 +31,76 @@ const getUsers = async ({ prisma, userLoader }: Context, resolveInfo: GraphQLRes
 }
 const developUsers = async ({ dto: data }: { dto: AboutUser }, { prisma }: Context) => {
     //позволяет пользователям создавать новых пользователей
-    try {
-        const user = await prisma.user.create({ data })
-        return user
-    } catch {
-        throw Error('operation failed')
-    }
+    return new Promise((resolve, reject) => {
+        try {
+            const user = prisma.user.create({ data })
+            resolve(user)
+        } catch {
+            reject('operation failed')
+        }
+    })
+
 }
 const modifyUsers = async ({ id, dto: data }: ID & { dto: Partial<AboutUser> }, { prisma }: Context) => {
     //пользователям обновлять существующих пользователей
-    try {
-        const user = await prisma.user.update({
-            where: { id }, //идентификатор пользователя, которого нужно обновить
-            data //содержит изменения, которые необходимо внести в информацию о пользователе
-        })
-        return user
-    } catch {
-        throw Error('operation failed')
-    }
+    return new Promise((resolve, reject) => {
+        try {
+            const user = prisma.user.update({
+                where: { id }, //идентификатор пользователя, которого нужно обновить
+                data //содержит изменения, которые необходимо внести в информацию о пользователе
+            })
+            resolve(user)
+        } catch {
+            reject('operation failed')
+        }
+    })
+
 }
+
+//     //позволяет пользователям удалять существующих пользователей
 const deleteUsers = async ({ id }: ID, { prisma }: Context) => {
-    //позволяет пользователям удалять существующих пользователей
+    console.log('deleting');
     try {
         await prisma.user.delete({
-            where: { id } //является идентификатором удаляемого пользователя
-        })
-        return id
+            where: { id } // идентификатор удаляемого пользователя
+        });
+        return (id);
     } catch {
-        throw Error('operation failed')
+        throw new Error('operation failed');
     }
-}
+};
+
 const subscribedTo = async ({ userId: id, authorId }: SubscriptionMutationUser, { prisma }: Context) => {
     //позволяет пользователям подписываться на других пользователей
-    try {
-        const user = await prisma.user.update({
-            where: { id },//это идентификатор пользователя, который подписывается
-            data: { userSubscribedTo: { create: { authorId } } },//это идентификатор пользователя, на которого подписывается пользователь
-        })
-        return user
-    } catch {
-        throw Error('operation failed')
-    }
-
+    return new Promise((resolve, reject) => {
+        try {
+            const user = prisma.user.update({
+                where: { id },//это идентификатор пользователя, который подписывается
+                data: { userSubscribedTo: { create: { authorId } } },//это идентификатор пользователя, на которого подписывается пользователь
+            })
+            resolve(user)
+        } catch {
+            reject('operation failed')
+        }
+    })
 }
 const unSubscibedFrom = async ({ userId: subscriberId, authorId }: SubscriptionMutationUser, { prisma }: Context) => {
     //позволяет пользователям отписываться от других пользователей
-    try {
-        const user = await prisma.subscribersOnAuthors.delete({
-            where: {
-                subscriberId_authorId: {
-                    subscriberId, //идентификатор пользователя, отписавшегося от подписки
-                    authorId //идентификатор пользователя, от которого отписывается пользователь
-                }
-            },
-        })
-        return user
-    } catch {
-        throw Error('operation failed')
-    }
+    return new Promise((resolve, reject) => {
+        try {
+            const user = prisma.subscribersOnAuthors.delete({
+                where: {
+                    subscriberId_authorId: {
+                        subscriberId, //идентификатор пользователя, отписавшегося от подписки
+                        authorId //идентификатор пользователя, от которого отписывается пользователь
+                    }
+                },
+            })
+            resolve(user)
+        } catch {
+            reject('operation failed')
+        }
+    })
 }
 export default {
     user: getUser,
