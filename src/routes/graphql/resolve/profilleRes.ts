0569d50a4df3d3d4e48ea58d2b214356ba143ddb
aliya <tusupkalievaaliya921@gmail.com>
2023-07-27 15:17:39 +0500

    feat:added resProfile which manage profiles

---
+++ src/routes/graphql/resolve/profilleRes.ts
@@ -0,0 +1,53 @@
+import { Context, ID } from "../types/subscription.js";
+import { memProfile } from "../types/Profile.js";
+import { Profile } from "@prisma/client";
+
+const getThisProfile = async ({ id }: ID, { prisma }: Context) => {
+    const profile = await prisma.profile.findUnique({
+        where: { id }
+    })
+    console.log(profile)
+}
+
+const getThisProfiles = async ({ prisma }: Context) => {
+    const allprofiles = await prisma.profile.findMany()
+    return JSON.stringify(allprofiles);
+}
+
+const developProfile = async ({ dto: data }: { dto: Profile }, { prisma }: Context) => {
+    try {
+        const profile = await prisma.profile.create({ data });
+        console.log(profile)
+    } catch {
+        throw new Error('file not found')
+    }
+};
+
+const modifyProfile = async ({ id }: ID, { dto: data }: { dto: memProfile }, { prisma }: Context) => {
+    try {
+        const profile = await prisma.profile.update({
+            where: { id },
+            data
+        })
+        console.log(profile)
+    } catch {
+        throw new Error('file not found')
+    }
+}
+const deletedProfile = async ({ id }: ID, { prisma }: Context) => {
+    try {
+        const profile = await prisma.profile.delete({
+            where: { id },
+        })
+        console.log(profile)
+    } catch {
+        throw new Error('operation is failed')
+    }
+}
+export default {
+    getThisProfile,
+    getThisProfiles,
+    developProfile,
+    modifyProfile,
+    deletedProfile,
+}
