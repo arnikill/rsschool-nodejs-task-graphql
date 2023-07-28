b5bdb8fe84465807ea31601eef7f346d55be0d33
aliya <tusupkalievaaliya921@gmail.com>
2023-07-28 07:56:15 +0500

    feat: add GraphQL plugin for Fastify server

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,22 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { createGqlResponseSchema, gqlResponseSchema, gqlSchema } from './schemas.js';
+import depthLimit from 'graphql-depth-limit'
+import { developDataLoaders } from './loader.js';
+import { graphql, validate, parse } from 'graphql';
+import memberRes from './resolve/memberRes.js';
+import postRes from './resolve/postRes.js';
+import profilleRes from './resolve/profilleRes.js';
+import userResolve from './resolve/userResolve.js';
 
+const rootValue = {
+  ...userResolve,
+  ...memberRes,
+  ...postRes,
+  ...profilleRes,
+}
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const { prisma } = fastify;
+  const dataLoaders = developDataLoaders(prisma)
   fastify.route({
     url: '/',
     method: 'POST',
@@ -13,7 +27,20 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      const errors = validate(gqlSchema, parse(req.body.query), [depthLimit(5)])
+
+      if (errors.length > 0) {
+        return { errors };
+      }
+
+      const response = await graphql({
+        schema: gqlSchema,
+        source: req.body.query,
+        rootValue,
+        variableValues: req.body.variables,
+        contextValue: { prisma, ...dataLoaders }
+      })
+      return response;
     },
   });
 };
